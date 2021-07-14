0576ef5b6437678bf23a1a81f25eb1aca2f63998
Moocow9m <devmoocow9m@gmail.com>
2021-07-14 15:09:59 -0700

    update kotlin and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.5.21"
 }
 
 group = "me.camdenorrb"
@@ -11,14 +11,20 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.capnproto:runtime:0.1.5")
+    implementation("org.capnproto:runtime:0.1.8")
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        sourceCompatibility = JavaVersion.VERSION_16.toString()
+        targetCompatibility = JavaVersion.VERSION_16.toString()
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_16.toString()
+        kotlinOptions.languageVersion = "1.6"
+        kotlinOptions.apiVersion = "1.6"
     }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+
+    withType<JavaCompile> {
+        sourceCompatibility = JavaVersion.VERSION_16.toString()
+        targetCompatibility = JavaVersion.VERSION_16.toString()
     }
 }
