fab4a51ae340b4c75eab44bf60aa1fd27b7473f9
Moocow9m <devmoocow9m@gmail.com>
2021-07-15 14:04:12 -0700

    simplify number stack

--- src/main/kotlin/me/camdenorrb/crescentvm/vm/stack/on/numbers/StackFloat.kt
+++ src/main/kotlin/me/camdenorrb/crescentvm/vm/stack/on/numbers/StackFloat.kt
@@ -1,33 +1,3 @@
 package me.camdenorrb.crescentvm.vm.stack.on.numbers
 
-import me.camdenorrb.crescentvm.vm.stack.on.OnStack
-
-object StackFloat: Number(), OnStack {
-    override fun toByte(): Byte {
-        TODO("Not yet implemented")
-    }
-
-    override fun toChar(): Char {
-        TODO("Not yet implemented")
-    }
-
-    override fun toDouble(): Double {
-        TODO("Not yet implemented")
-    }
-
-    override fun toFloat(): Float {
-        TODO("Not yet implemented")
-    }
-
-    override fun toInt(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun toLong(): Long {
-        TODO("Not yet implemented")
-    }
-
-    override fun toShort(): Short {
-        TODO("Not yet implemented")
-    }
-}
+object StackFloat : StackNumber()
---
+++ src/main/kotlin/me/camdenorrb/crescentvm/vm/stack/on/numbers/StackNumber.kt
@@ -0,0 +1,33 @@
+package me.camdenorrb.crescentvm.vm.stack.on.numbers
+
+import me.camdenorrb.crescentvm.vm.stack.on.OnStack
+
+open class StackNumber : Number(), OnStack {
+    override fun toByte(): Byte {
+        TODO("Not yet implemented")
+    }
+
+    override fun toChar(): Char {
+        TODO("Not yet implemented")
+    }
+
+    override fun toDouble(): Double {
+        TODO("Not yet implemented")
+    }
+
+    override fun toFloat(): Float {
+        TODO("Not yet implemented")
+    }
+
+    override fun toInt(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun toLong(): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun toShort(): Short {
+        TODO("Not yet implemented")
+    }
+}
