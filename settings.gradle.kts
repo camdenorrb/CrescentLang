d9bc4bfe6d6e74d9f98a82def690111e67b01271
camdenorrb <me@12oclock.dev>
2022-01-02 04:55:24 -0600

    Remove PoderIR source reliance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
 rootProject.name = "CrescentLang"
 
+/*
 sourceControl {
     gitRepository(java.net.URI.create("https://github.com/Moocow9m/PoderTechIR.git")) {
         producesModule("tech.poder.ir:PoderTechIR")
     }
-}
+}*/
