9dbf792476edb3685cc7d03a82b6d0b311ff6ff2
Moocow9m <devmoocow9m@gmail.com>
2022-01-05 19:48:48 -0800

    Start trans Stub

--- src/main/kotlin/dev/twelveoclock/lang/crescent/translators/CresentToPTIR.kt
+++ src/main/kotlin/dev/twelveoclock/lang/crescent/translators/CresentToPTIR.kt
@@ -2,15 +2,50 @@ package dev.twelveoclock.lang.crescent.translators
 
 import dev.twelveoclock.lang.crescent.language.ast.CrescentAST
 import tech.poder.ir.api.CodeFile
+import java.nio.file.Path
 
 object CresentToPTIR {
-	private val environment = mutableMapOf<String, CodeFile>()
+	private val environmentMethods = mutableMapOf<String, UInt>()
+	private val environmentFiles = mutableMapOf<String, CodeFile>()
 
 	fun resetEnv() {
-		environment.clear()
+		environmentFiles.clear()
+		environmentMethods.clear()
 	}
 
-	fun translate(crescent: CrescentAST.Node.File): CodeFile {
-		TODO()
+	fun translate(projectDir: Path, vararg crescent: CrescentAST.Node.File): List<CodeFile> {
+		val exec = mutableMapOf<String, CrescentAST.Node.File>()
+		crescent.forEach {
+			val file = CodeFile(projectDir.relativize(it.path).toString())
+			check(!environmentFiles.containsKey(file.name)) {
+				"File ${file.name} already exists in environment!"
+			}
+			environmentFiles[file.name] = file
+			it.functions.forEach { (_, u) ->
+				val name = file.name + "F" + u.name
+				check(!environmentMethods.containsKey(name)) {
+					"Function $name already exists in environment!"
+				}
+				val method = file.addMethodStub()
+				environmentMethods[name] = method
+			}
+			if (it.mainFunction !== null) {
+				val mainMethod = file.addMethodStub()
+				val name = file.name + "M" + it.mainFunction.name
+				check(!environmentMethods.containsKey(name)) {
+					"Function $name already exists in environment!"
+				}
+				environmentMethods[name] = mainMethod
+			}
+			exec[it.path.toString()] = it
+		}
+		val result = mutableListOf<CodeFile>()
+		exec.forEach { t, u ->
+			val file = environmentFiles[t]!!
+			TODO()
+			result.add(file)
+		}
+
+		return result
 	}
 }
---
+++ src/test/kotlin/dev/twelveoclock/lang/crescent/ptir/TranslatorTest.kt
@@ -0,0 +1,19 @@
+package dev.twelveoclock.lang.crescent.ptir
+
+import dev.twelveoclock.lang.crescent.data.TestCode
+import dev.twelveoclock.lang.crescent.lexers.CrescentLexer
+import dev.twelveoclock.lang.crescent.parsers.CrescentParser
+import dev.twelveoclock.lang.crescent.translators.CresentToPTIR
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+import kotlin.io.path.Path
+
+class TranslatorTest {
+	/*@Test
+	fun helloWorld() {
+		val file = CrescentParser.invoke(Path("Test", "example.crescent"), CrescentLexer.invoke(TestCode.helloWorlds))
+		CresentToPTIR.resetEnv()
+		val code = CresentToPTIR.translate(Paths.get("Test"), file)
+		println(code)
+	}*/
+}
